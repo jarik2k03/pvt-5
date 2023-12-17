0a6339e195532628283d3513198c0c78d79bda6f
Yartanis <yartanis@Drakkar>
2023-12-17 18:40:14 +0300

    prepare parallel

--- cursovoj/pgauss.cxx
+++ cursovoj/pgauss.cxx
@@ -1,8 +1,15 @@
 #include <math.h>
-#include "../mpi.h"
+#include <mpi.h>
+#include <sys/time.h>
 #include <iostream>
 #include <vector>
 using namespace std;
+
+double wtime() {
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (double)t.tv_sec + (double)t.tv_usec * 1E-6;
+}
 
 void printMatrixValues(float** arr, int n, int m) {
   for (int i = 0; i < n; ++i) {
@@ -23,32 +30,7 @@ void printInverseMatrix(float** arr, int n, int m) {
   }
   cout << '\n';
 }
-
-void get_chunk(int a, int b, int commsize, int rank, int *lb, int *ub) {
-    int n = b - a + 1;
-    int q = n / commsize;
-    if (n % commsize) q++;
-    int r = commsize * q - n;
-    
-    /* Compute chunk size for the process */
-    int chunk = q;
-    if (rank >= commsize - r) chunk = q - 1;
-    *lb = a; /* Determine start item for the process */
-    
-    if (rank > 0) { /* Count sum of previous chunks */
-        if (rank <= commsize - r)
-            *lb += q * rank;
-        else
-            *lb += q * (commsize - r) + (q - 1) * (rank - (commsize - r));
-    }
-    *ub = *lb + chunk - 1;
-}
-
 void findInvMatGaussJordan(float** mat, int order) {
-  int commrank, commsize;
-  MPI_Comm_rank(MPI_COMM_WORLD, &commrank);
-  MPI_Comm_size(MPI_COMM_WORLD, &commsize);
-  
   float temp = 0.0f;
 
   // printMatrixValues(mat, order, order);
@@ -61,7 +43,7 @@ void findInvMatGaussJordan(float** mat, int order) {
   // printMatrixValues(mat, order, order);
   // printInverseMatrix(mat, order, 2 * order);
   //  сортировка по 0 элементу
-  double time = MPI_Wtime();
+  double time = wtime();
   for (int i = order - 1; i > 0; --i) {
     if (mat[i - 1][0] < mat[i][0]) {
       float* temp = mat[i];
@@ -69,41 +51,35 @@ void findInvMatGaussJordan(float** mat, int order) {
       mat[i - 1] = temp;
     }
   }
-  cout << "Sorted matrix: " << MPI_Wtime() - time << " sec.\n";
+  cout << "Sorted matrix: " << wtime() - time << " sec.\n";
   // printMatrixValues(mat, order, order);
   // printInverseMatrix(mat, order, 2 * order);
   //  вычитание строк
-  int lb, ub;
-  get_chunk(0, order - 1, commsize, commrank, &lb, &ub);
-  time = MPI_Wtime();
+  time = wtime();
   for (int i = 0; i < order; ++i) {
     for (int j = 0; j < order; ++j) {
       if (j == i)
-        continue; 
+        continue;
       temp = mat[j][i] / mat[i][i];
       for (int k = 0; k < 2 * order; ++k) {
         mat[j][k] -= mat[i][k] * temp;
       }
-      
     }
   }
-  cout << "Processed matrix: " << MPI_Wtime() - time << " sec.\n";
+  cout << "Processed matrix: " << wtime() - time << " sec.\n";
   // printMatrixValues(mat, order, order);
   // printInverseMatrix(mat, order, 2 * order);
   //  деление для получения единичной
-  time = MPI_Wtime();
+  time = wtime();
   for (int i = 0; i < order; ++i) {
     temp = mat[i][i];
     for (int j = 0; j < 2 * order; ++j) {
       mat[i][j] = mat[i][j] / temp;
     }
   }
-  cout << "Divided matrix: " << MPI_Wtime() - time << " sec.\n";
-  if (commrank == 0) {
-    printMatrixValues(mat, order, order);
-    printInverseMatrix(mat, order, 2 * order);
-  }
-
+  cout << "Divided matrix: " << wtime() - time << " sec.\n";
+  // printMatrixValues(mat, order, order);
+  // printInverseMatrix(mat, order, 2 * order);
 }
 int main(int argc, char** argv) {
   
@@ -113,28 +89,33 @@ int main(int argc, char** argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &commsize);
 
   int order = (argc > 1 && argv[1] != nullptr) ? atoi(argv[1]) : 3;
-
-  int lb, ub; // Декомпозиция матрицы на горизонтальные полосы
-  get_chunk(0, order - 1, commsize, commrank, &lb, &ub);
-  int nrows = ub - lb + 1;
-  cout << "NROWS : " << nrows << " UB: " << ub << " LB: " << lb << '\n';
-
-  int matrix_size = (nrows * order) << 1;
-  float** mat = new float*[nrows];
+  int elements_per_proc = order / commsize;
+  float** mat = new float*[elements_per_proc];
   for (int i = 0; i < order; ++i)
     mat[i] = new float[order * 2];
 
-  for (int i = 0; i < nrows; ++i) {
+  for (int i = 0; i < elements_per_proc; ++i) {
     for (int j = 0; j < order; ++j) {
       mat[i][j] = rand() % 16;
     }
   }
-  findInvMatGaussJordan(mat, order);
 
-  
-  for (int i = 0; i < order; ++i)
+  MPI_Scatter(mat, elements_per_proc * (2 * order), MPI_FLOAT, mat,
+              elements_per_proc * (2 * order), MPI_FLOAT, 0,
+              MPI_COMM_WORLD);
+  double t = MPI_Wtime();
+  findInvMatGaussJordan(mat, elements_per_proc);
+  cout << MPI_Wtime() - t; 
+    
+  double time;
+  MPI_Reduce(&t, &time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+  if (commrank == 0) {
+    cout << time << '\n';
+  }
+
+  for (int i = 0; i < elements_per_proc; ++i)
     delete[] mat[i];
-  MPI_Finalize();
-  
+  delete[] mat;
+
   return 0;
 }
